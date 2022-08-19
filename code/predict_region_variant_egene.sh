ea56ef32b6d9e14279d14081faff22afdba4570c
xixi-cathy <57483791+xixi-cathy@users.noreply.github.com>
2022-08-19 13:14:05 +0800

    Delete code directory

--- code/predict_region_variant_egene.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export OPENBLAS_NUM_THREADS=1
-export GOTO_NUM_THREADS=1
-export OMP_NUM_THREADS=1
-python causal_regions_variants.py
-Rscript causal_egenes.R
--- code/predict_regulation_path.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python link_cell_type.py
-Rscript regulation_path.R
